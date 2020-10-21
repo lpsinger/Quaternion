7cae9d989a340258981dd9aab9745b26b79c0f58
Javier Gonzalez <javierggt@yahoo.com>
2020-10-21 14:22:35 -0400

    fixes until the package description looked ok

--- setup.py
+++ setup.py
@@ -6,16 +6,38 @@ try:
 except ImportError:
     cmdclass = {}
 
+
+long_description = """Quaternion provides a class for manipulating quaternion objects.
+
+To install:
+
+```
+pip install quaternion
+```
+
+To run some tests:
+
+```
+pip install pytest
+python -c 'import Quaternion; Quaternion.test()'
+```
+
+Documentation: https://sot.github.io/Quaternion
+
+Source: https://github.com/sot/Quaternion
+"""
+
 setup(name='Quaternion',
       author='Jean Connelly',
       description='Quaternion object manipulation',
+      long_description_content_type='text/markdown',
+      long_description=long_description,
       author_email='jconnelly@cfa.harvard.edu',
       packages=['Quaternion', 'Quaternion.tests'],
-      license=("New BSD/3-clause BSD License\nCopyright (c) 2016"
-               " Smithsonian Astrophysical Observatory\nAll rights reserved."),
+      license="New BSD/3-clause BSD License. Copyright (c) 2016 Smithsonian Astrophysical Observatory. All rights reserved.",
       download_url='http://pypi.python.org/pypi/Quaternion/',
-      url='http://cxc.harvard.edu/mta/ASPECT/tool_doc/pydocs/Quaternion.html',
-      version='3.5.2',
+      url='https://sot.github.io/Quaternion',
+      version='3.5.2.post4',
       zip_safe=False,
       tests_require=['pytest'],
       package_data={'Quaternion.tests': ['data/*.pkl']},
